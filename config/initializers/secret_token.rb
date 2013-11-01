1699e32dfd74a8d987d89852e13372091628431a
Orkish <winson.yuan@hotmail.com>
2013-11-01 13:44:41 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OffenderApp::Application.config.secret_key_base = 'b95c17765d97e8e906c5132c0bc0ae825225c8331d54aca741c1fb428320cc31345d42205c26e5b73ecceb81b62705ee199a377efbf4920817af0bb490087156'
