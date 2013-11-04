a063ffab5a599bbb6739c8714dc24a8d9ad6a3dd
whyu <wesleyhl.yu@gmail.com>
2013-11-03 21:36:42 -0500

    finished completed seed file. adds offenders.json to the root folder

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,8 +1,8 @@
-# class Area
-#   include Mongoid::Document
-#   field :name, type: String
-#   field :children, type: Array, default: []
+class Area
+  include Mongoid::Document
+  field :name, type: String
+  field :children, type: Array, default: []
 
-#   has_many :zipcodes
-#   belongs_to :city
-# end
+  has_many :zipcodes
+  belongs_to :city
+end
